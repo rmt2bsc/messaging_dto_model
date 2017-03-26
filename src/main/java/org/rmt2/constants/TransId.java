63fab8415ceb09ec4420b5ed0e201fec528b7a33
appdev <royterrell2@gmail.com>
2017-03-26 16:04:08 -0500

    Created

---
+++ src/main/java/org/rmt2/constants/TransId.java
@@ -0,0 +1,18 @@
+package org.rmt2.constants;
+
+public class TransId {
+
+    // AddressBook
+    public static final String CONTACTS_BUSINESS_GET_ALL = "GET_ALL_BUSINESS_CONTACTS";
+    public static final String CONTACTS_BUSINESS_GET = "GET_BUSINESS_CONTACT";
+    public static final String CONTACTS_BUSINESS_GET_CRITERIA = "GET_BUSINESS_CONTACTS_USING_CRITERIA";
+
+    // MultiMedia
+    public static final String MEDIA_GET_CONTENT = "GET_CONTENT_ATTACHMENT";
+    public static final String MEDIA_SAVE_CONTENT = "SAVE_CONTENT_ATTACHMENT";
+
+    public TransId() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
