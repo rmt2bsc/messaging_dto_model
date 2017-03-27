29206a288ec2a33d307108a4b52ac02806b8a7a0
roy terrell <rmt2bsc@gmail.com>
2017-03-27 09:44:45 -0500

    Created

---
+++ src/main/java/org/rmt2/util/JaxbAddressBookFactory.java
@@ -0,0 +1,19 @@
+package org.rmt2.util;
+
+/**
+ * A factory for creating AddressBook related JAXB objects.
+ * 
+ * @author Roy Terrell
+ * 
+ */
+public class JaxbAddressBookFactory extends JaxbPayloadFactory {
+
+    /**
+     * Creates a JaxbAddressBookFactory object.
+     */
+    private JaxbAddressBookFactory() {
+        super();
+    }
+
+
+}
