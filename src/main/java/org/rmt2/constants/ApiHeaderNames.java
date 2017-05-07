4d2b201262c862afeb7c507ab6828d8cff498d0e
RMT2 Business Systems Corp <rmt2bsc@gmail.com>
2017-05-07 03:13:53 -0500

    Added messaging constants that are accessible to all projects and made other miscellaneous improvements.
    * Created.
    
    * Added.
    
    * Added
    
    * Added Cerner style exclusions
    
    * Delete .classpath
    
    * Delete .project

---
+++ src/main/java/org/rmt2/constants/ApiHeaderNames.java
@@ -0,0 +1,16 @@
+package org.rmt2.constants;
+
+/**
+ * Constant names contained in the header of the a typical RMT2 Message model
+ * 
+ * @author royterrell
+ *
+ */
+public class ApiHeaderNames {
+    public static final String APPLICATION = "application";
+
+    public static final String MODULE = "module";
+
+    public static final String TRANSACTION = "transaction";
+
+}
