02f19e46b8b77cefcda4494fa280d66e761fd663
DHISCO\roy.terrell <roy.terrell@dhisco.com>
2018-03-05 17:09:15 -0600

    CA-56: Added more header related constants.

--- src/main/java/org/rmt2/constants/ApiHeaderNames.java
+++ src/main/java/org/rmt2/constants/ApiHeaderNames.java
@@ -12,5 +12,11 @@ public class ApiHeaderNames {
     public static final String MODULE = "module";
 
     public static final String TRANSACTION = "transaction";
+    
+    public static final String MESSAGE_MODE_REQUEST = "REQUEST";
+    
+    public static final String MESSAGE_MODE_RESPONSE = "RESPONSE";
+    
+    public static final String DUMMY_HEADER_VALUE = "XXXXXXX";
 
 }
