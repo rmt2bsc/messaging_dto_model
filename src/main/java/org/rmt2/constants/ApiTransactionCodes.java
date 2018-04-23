a1624d405ba2a6e558b1d96e5af50091f47011bf
DHISCO\roy.terrell <roy.terrell@dhisco.com>
2018-04-23 11:51:16 -0500

    CA-27: Added MEDIA_CONTENT_APP_LINK constant which represents the id of the request, MediaApplicationLinkRequest.

--- src/main/java/org/rmt2/constants/ApiTransactionCodes.java
+++ src/main/java/org/rmt2/constants/ApiTransactionCodes.java
@@ -13,6 +13,7 @@ public class ApiTransactionCodes {
     // MultiMedia
     public static final String MEDIA_GET_CONTENT = "GET_CONTENT_ATTACHMENT";
     public static final String MEDIA_SAVE_CONTENT = "SAVE_CONTENT_ATTACHMENT";
+    public static final String MEDIA_CONTENT_APP_LINK = "LINK_CONTENT_TO_APPLICATION";
 
     // Accounting - Sales
     public static final String ACCOUNTING_SALESORDER_CREATE = "CREATE_SALES_ORDER";
