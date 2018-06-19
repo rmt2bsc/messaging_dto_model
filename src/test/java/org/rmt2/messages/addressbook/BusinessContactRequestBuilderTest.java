b6fab88b4efa050f5b786eb7b946356c9e0328df
DHISCO\roy.terrell <roy.terrell@dhisco.com>
2018-06-19 14:21:58 -0500

    IS-3: Created

---
+++ src/test/java/org/rmt2/messages/addressbook/BusinessContactRequestBuilderTest.java
@@ -0,0 +1,67 @@
+package org.rmt2.messages.addressbook;
+
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.rmt2.constants.ApiHeaderNames;
+import org.rmt2.constants.MessagingConstants;
+import org.rmt2.jaxb.AddressBookRequest;
+import org.rmt2.jaxb.BusinessContactCriteria;
+import org.rmt2.jaxb.ContactCriteriaGroup;
+import org.rmt2.jaxb.HeaderType;
+import org.rmt2.jaxb.ObjectFactory;
+import org.rmt2.util.HeaderTypeBuilder;
+
+import com.api.config.ConfigConstants;
+import com.api.config.SystemConfigurator;
+import com.api.xml.jaxb.JaxbUtil;
+
+public class BusinessContactRequestBuilderTest {
+
+    private JaxbUtil jaxb;
+    
+    @Before
+    public void setUp() throws Exception {
+        try {
+            jaxb = SystemConfigurator.getJaxb(ConfigConstants.JAXB_CONTEXNAME_DEFAULT);
+        }
+        catch (Exception e) {
+            jaxb = new JaxbUtil(MessagingConstants.JAXB_RMT2_PKG);
+        }
+    }
+    
+    @Test
+    public void testBuildBusinessContactQueryRequest() {
+        ObjectFactory fact = new ObjectFactory();
+        AddressBookRequest req = fact.createAddressBookRequest();
+        
+        HeaderType head =  HeaderTypeBuilder.Builder.create()
+                .withApplication("App Name")
+                .withModule(ConfigConstants.API_APP_MODULE_VALUE)
+                .withMessageMode(ApiHeaderNames.MESSAGE_MODE_REQUEST)
+                .withDeliveryDate(new Date())
+                
+                // Set these header elements with dummy values in order to be properly assigned later.
+                .withTransaction(ApiHeaderNames.DUMMY_HEADER_VALUE)
+                .withRouting(ApiHeaderNames.DUMMY_HEADER_VALUE)
+                .withDeliveryMode(ApiHeaderNames.DUMMY_HEADER_VALUE).build();
+        
+        ContactCriteriaGroup ccg = fact.createContactCriteriaGroup();
+        BusinessContactCriteria criteria = fact.createBusinessContactCriteria();
+        criteria.setBusinessName("ABC Company");
+        criteria.setContactFname("john");
+        criteria.setContactLname("smith");
+        criteria.setContactEmail("johnn.smith@gte.net");
+        ccg.setBusinessCriteria(criteria);
+        req.setCriteria(ccg);
+        req.setHeader(head);
+        
+        String xml = jaxb.marshalJsonMessage(req);
+        System.out.println(xml);
+        Assert.assertNotNull(xml);
+        Assert.assertTrue(xml.contains("ABC Company"));
+    }
+ 
+}
