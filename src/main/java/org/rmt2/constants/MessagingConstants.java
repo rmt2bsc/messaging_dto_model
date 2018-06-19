204a06e185efab34b19a73579647aecd6474ed99
DHISCO\roy.terrell <roy.terrell@dhisco.com>
2018-06-19 14:21:31 -0500

    IS-3: Created

---
+++ src/main/java/org/rmt2/constants/MessagingConstants.java
@@ -0,0 +1,16 @@
+package org.rmt2.constants;
+
+/**
+ * 
+ * @author roy.terrell
+ *
+ */
+public class MessagingConstants {
+
+    public static final String JAXB_RMT2_PKG = "org.rmt2.jaxb";
+    
+    public MessagingConstants() {
+
+    }
+
+}
