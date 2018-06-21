20e5cb2f3f1f70c1e9e804c23881ae1e5f8f2a51
DHISCO\roy.terrell <roy.terrell@dhisco.com>
2018-06-21 17:47:55 -0500

    IS-3: Added update request test

--- src/test/java/org/rmt2/messages/addressbook/BusinessContactRequestBuilderTest.java
+++ src/test/java/org/rmt2/messages/addressbook/BusinessContactRequestBuilderTest.java
@@ -1,17 +1,23 @@
 package org.rmt2.messages.addressbook;
 
+import java.math.BigInteger;
 import java.util.Date;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.rmt2.constants.ApiHeaderNames;
+import org.rmt2.constants.ApiTransactionCodes;
 import org.rmt2.constants.MessagingConstants;
 import org.rmt2.jaxb.AddressBookRequest;
+import org.rmt2.jaxb.AddressType;
 import org.rmt2.jaxb.BusinessContactCriteria;
+import org.rmt2.jaxb.BusinessType;
 import org.rmt2.jaxb.ContactCriteriaGroup;
+import org.rmt2.jaxb.ContactDetailGroup;
 import org.rmt2.jaxb.HeaderType;
 import org.rmt2.jaxb.ObjectFactory;
+import org.rmt2.jaxb.ZipcodeType;
 import org.rmt2.util.HeaderTypeBuilder;
 
 import com.api.config.ConfigConstants;
@@ -44,7 +50,7 @@ public class BusinessContactRequestBuilderTest {
                 .withDeliveryDate(new Date())
                 
                 // Set these header elements with dummy values in order to be properly assigned later.
-                .withTransaction(ApiHeaderNames.DUMMY_HEADER_VALUE)
+                .withTransaction(ApiTransactionCodes.CONTACTS_GET)
                 .withRouting(ApiHeaderNames.DUMMY_HEADER_VALUE)
                 .withDeliveryMode(ApiHeaderNames.DUMMY_HEADER_VALUE).build();
         
@@ -64,4 +70,58 @@ public class BusinessContactRequestBuilderTest {
         Assert.assertTrue(xml.contains("ABC Company"));
     }
  
+    
+    @Test
+    public void testBuildBusinessContactUpdateRequest() {
+        ObjectFactory fact = new ObjectFactory();
+        AddressBookRequest req = fact.createAddressBookRequest();
+        
+        HeaderType head =  HeaderTypeBuilder.Builder.create()
+                .withApplication("App Name")
+                .withModule(ConfigConstants.API_APP_MODULE_VALUE)
+                .withMessageMode(ApiHeaderNames.MESSAGE_MODE_REQUEST)
+                .withDeliveryDate(new Date())
+                
+                // Set these header elements with dummy values in order to be properly assigned later.
+                .withTransaction(ApiTransactionCodes.CONTACTS_UPDATE)
+                .withRouting(ApiHeaderNames.DUMMY_HEADER_VALUE)
+                .withDeliveryMode(ApiHeaderNames.DUMMY_HEADER_VALUE).build();
+        
+        ContactDetailGroup cdg = fact.createContactDetailGroup();
+        
+        BusinessType obj = fact.createBusinessType();
+        obj.setBusinessId(BigInteger.valueOf(1350));
+        obj.setLongName("ABC Company");
+        obj.setContactFirstname("john");
+        obj.setContactLastname("smith");
+        obj.setContactEmail("johnn.smith@gte.net");
+        obj.setContactPhone("2149999999");
+        obj.setTaxId("88-8888888");
+        
+        AddressType addr = fact.createAddressType();
+        addr.setAddrId(BigInteger.valueOf(1000));
+        addr.setAddr1("Address Line 1");
+        addr.setAddr2("Address Line 2");
+        addr.setAddr3("Address Line 3");
+        addr.setAddr4("Address Line 4");
+        addr.setPhoneMain("2147777777");
+        
+        ZipcodeType zip = fact.createZipcodeType();
+        zip.setCity("Dallas");
+        zip.setState("Tx");
+        zip.setZipcode(BigInteger.valueOf(75232));
+        
+        addr.setZip(zip);
+        obj.setAddress(addr);
+        
+        cdg.getBusinessContacts().add(obj);
+        
+        req.setProfile(cdg);
+        req.setHeader(head);
+        
+        String xml = jaxb.marshalJsonMessage(req);
+        System.out.println(xml);
+        Assert.assertNotNull(xml);
+        Assert.assertTrue(xml.contains("ABC Company"));
+    }
 }
