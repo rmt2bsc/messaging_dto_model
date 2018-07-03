5023100204e8a8220f67578640c3c7a8a1ba3aaa
DHISCO\roy.terrell <roy.terrell@dhisco.com>
2018-07-03 17:22:17 -0500

    IS-3: Added methods, testBuildCountryQueryRequest and testBuildRegionQueryRequest.

--- src/test/java/org/rmt2/messages/addressbook/PostalRequestBuilderTest.java
+++ src/test/java/org/rmt2/messages/addressbook/PostalRequestBuilderTest.java
@@ -9,10 +9,12 @@ import org.junit.Test;
 import org.rmt2.constants.ApiHeaderNames;
 import org.rmt2.constants.ApiTransactionCodes;
 import org.rmt2.constants.MessagingConstants;
+import org.rmt2.jaxb.CountryCriteriaType;
 import org.rmt2.jaxb.HeaderType;
 import org.rmt2.jaxb.ObjectFactory;
 import org.rmt2.jaxb.PostalRequest;
 import org.rmt2.jaxb.PostalRequest.PostalCriteria;
+import org.rmt2.jaxb.StatesCriteriaType;
 import org.rmt2.jaxb.ZipResultFormatType;
 import org.rmt2.jaxb.ZipcodeCriteriaType;
 import org.rmt2.util.HeaderTypeBuilder;
@@ -36,7 +38,7 @@ public class PostalRequestBuilderTest {
     }
     
     @Test
-    public void testBuildLookupGroupQueryRequest() {
+    public void testBuildZipcodeQueryRequest() {
         ObjectFactory fact = new ObjectFactory();
         PostalRequest req = fact.createPostalRequest();
         
@@ -69,4 +71,65 @@ public class PostalRequestBuilderTest {
         Assert.assertTrue(xml.contains("FULL"));
     }
  
+    @Test
+    public void testBuildCountryQueryRequest() {
+        ObjectFactory fact = new ObjectFactory();
+        PostalRequest req = fact.createPostalRequest();
+        
+        HeaderType head =  HeaderTypeBuilder.Builder.create()
+                .withApplication("addressbook")
+                .withModule("postal")
+                .withMessageMode(ApiHeaderNames.MESSAGE_MODE_REQUEST)
+                .withDeliveryDate(new Date())
+                
+                // Set these header elements with dummy values in order to be properly assigned later.
+                .withTransaction(ApiTransactionCodes.COUNTRY_GET)
+                .withRouting(ApiHeaderNames.DUMMY_HEADER_VALUE)
+                .withDeliveryMode(ApiHeaderNames.DUMMY_HEADER_VALUE).build();
+        
+        PostalCriteria postalCriteria = fact.createPostalRequestPostalCriteria();
+        CountryCriteriaType criteria = fact.createCountryCriteriaType();
+        criteria.setCountryId(BigInteger.valueOf(12345));
+        criteria.setCountyName("United States");
+        postalCriteria.setCountry(criteria);
+        req.setPostalCriteria(postalCriteria);
+        req.setHeader(head);
+        
+        String xml = jaxb.marshalJsonMessage(req);
+        System.out.println(xml);
+        Assert.assertNotNull(xml);
+        Assert.assertTrue(xml.contains("United States"));
+    }
+    
+    @Test
+    public void testBuildRegionQueryRequest() {
+        ObjectFactory fact = new ObjectFactory();
+        PostalRequest req = fact.createPostalRequest();
+        
+        HeaderType head =  HeaderTypeBuilder.Builder.create()
+                .withApplication("addressbook")
+                .withModule("postal")
+                .withMessageMode(ApiHeaderNames.MESSAGE_MODE_REQUEST)
+                .withDeliveryDate(new Date())
+                
+                // Set these header elements with dummy values in order to be properly assigned later.
+                .withTransaction(ApiTransactionCodes.REGION_GET)
+                .withRouting(ApiHeaderNames.DUMMY_HEADER_VALUE)
+                .withDeliveryMode(ApiHeaderNames.DUMMY_HEADER_VALUE).build();
+        
+        PostalCriteria postalCriteria = fact.createPostalRequestPostalCriteria();
+        StatesCriteriaType criteria = fact.createStatesCriteriaType();
+        criteria.setCountryId(BigInteger.valueOf(12345));
+        criteria.setStateId(BigInteger.valueOf(2222));
+        criteria.setStateName("Texas");
+        criteria.setStateCode("TX");
+        postalCriteria.setProvince(criteria);
+        req.setPostalCriteria(postalCriteria);
+        req.setHeader(head);
+        
+        String xml = jaxb.marshalJsonMessage(req);
+        System.out.println(xml);
+        Assert.assertNotNull(xml);
+        Assert.assertTrue(xml.contains("Texas"));
+    }
 }
