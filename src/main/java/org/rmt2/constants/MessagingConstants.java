f9c95e86ceeaf7622dbd1c03b3b6f294e9cd547f
DHISCO\roy.terrell <roy.terrell@dhisco.com>
2018-07-05 18:48:58 -0500

    IS-3: Added constants to exist exactly as those in WebServiceConstants class of the fw_messaging project.  This is needed in cases where a project may need to make these references but is not dependent on the fw_messaging project.

--- src/main/java/org/rmt2/constants/MessagingConstants.java
+++ src/main/java/org/rmt2/constants/MessagingConstants.java
@@ -9,6 +9,37 @@ public class MessagingConstants {
 
     public static final String JAXB_RMT2_PKG = "org.rmt2.jaxb";
     
+    /**
+     * 
+     */
+    public static final String MSG_TRANSPORT_MODE_SYNC = "SYNC";
+
+    /**
+     * 
+     */
+    public static final String MSG_MODE_REQUEST = "REQUEST";
+
+    /**
+     * 
+     */
+    public static final String MSG_MODE_RESPONSE = "RESPONSE";
+
+    /**
+     * 
+     */
+    public static final String MSG_TRANSPORT_MODE_ASYNC = "ASYNC";
+
+    /**
+     * 
+     */
+    public static final String RETURN_STATUS_SUCCESS = "SUCCESS";
+
+    /**
+     * 
+     */
+    public static final String RETURN_STATUS_ERROR = "ERROR";
+
+    
     public MessagingConstants() {
 
     }
