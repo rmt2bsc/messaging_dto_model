527be0206049244de792567b5cc5659cab8c0a4c
DHISCO\roy.terrell <roy.terrell@dhisco.com>
2018-07-05 18:51:43 -0500

    IS-3: Copied methods that existed in the class, MessageHandlerHelper.java, since they were Messaging_DTO_Model specific.

--- src/main/java/org/rmt2/util/JaxbPayloadFactory.java
+++ src/main/java/org/rmt2/util/JaxbPayloadFactory.java
@@ -3,11 +3,14 @@ package org.rmt2.util;
 import java.math.BigInteger;
 import java.util.Date;
 
+import org.rmt2.constants.MessagingConstants;
 import org.rmt2.jaxb.HeaderType;
 import org.rmt2.jaxb.ObjectFactory;
 import org.rmt2.jaxb.RSCommonReply;
 import org.rmt2.jaxb.ReplyStatusType;
 
+import com.api.config.ConfigConstants;
+import com.api.config.SystemConfigurator;
 import com.api.util.RMT2Date;
 import com.api.xml.jaxb.JaxbUtil;
 
@@ -58,6 +61,25 @@ public class JaxbPayloadFactory {
         header.setUserId(userId);
         return header;
     }
+    
+    /**
+     * Create a reply status type object.
+     * 
+     * @param returnCode
+     * @param statusCode
+     * @param message
+     * @param extMessage
+     * @return
+     */
+    public static final ReplyStatusType createReplyStatus(int returnCode, String statusCode, String message, String extMessage) {
+        ReplyStatusType rs = ReplyStatusTypeBuilder.Builder.create()
+                .withReturnCode(returnCode)
+                .withStatus(statusCode.equals(MessagingConstants.RETURN_STATUS_SUCCESS) ? true : false)
+                .withMessage(message)
+                .withDetailMessage(extMessage)
+                .build();
+        return rs;
+    }
 
     /**
      * Creates a JAXB ReplyStatusType using <i>successfull</i>, <i>msg</i>,
@@ -101,6 +123,52 @@ public class JaxbPayloadFactory {
     }
 
     /**
+     * Build a common response reply which will represent an error message.
+     * 
+     * @param errMsg
+     * @param errCode
+     * @param app
+     * @param module
+     * @param trans
+     * @return an instance of {@link RSCommonReply}
+     */
+    public static final RSCommonReply buildCommonErrorResponseMessage(String errMsg, String extErrMsg, 
+            int errCode, String app, String module, String trans) {
+        ObjectFactory fact = new ObjectFactory();
+        HeaderType header = HeaderTypeBuilder.Builder.create()
+                .withApplication(app).withModule(module).withTransaction(trans)
+                .withDeliveryDate(new Date()).withRouting("Busiess Server")
+                .build();
+        
+        ReplyStatusType rs = createReplyStatus(errCode, MessagingConstants.RETURN_STATUS_ERROR, errMsg, extErrMsg);
+        RSCommonReply reply = fact.createRSCommonReply();
+        reply.setReplyStatus(rs);
+        reply.setHeader(header);
+        return reply;
+    }
+    
+    /**
+     * Build a common response reply, which will represent an error message, and
+     * return as an XML String.
+     * 
+     * @param errMsg
+     * @param errCode
+     * @param app
+     * @param module
+     * @param trans
+     * @return XML String equivalent of {@link RSCommonReply}
+     */
+    public static final String buildCommonErrorResponseMessageXml(String errMsg, String extErrMsg, 
+            int errCode, String app, String module, String trans) {
+        RSCommonReply reply = buildCommonErrorResponseMessage(errMsg, extErrMsg,
+                errCode, app, module, trans);
+        JaxbUtil jaxb = SystemConfigurator.getJaxb(ConfigConstants.JAXB_CONTEXNAME_DEFAULT);
+        String xml = jaxb.marshalJsonMessage(reply);
+        return xml;
+    }
+    
+    
+    /**
      * Produces a common web service reply representing a successful invocation.
      * 
      * @param entityId
@@ -113,8 +181,7 @@ public class JaxbPayloadFactory {
      * @throws DataServiceException
      *             general errors.
      */
-    public static final String buildCommonPayload(int entityId, String message,
-            String userId) {
+    public static final String buildCommonPayload(int entityId, String message, String userId) {
         ObjectFactory f = new ObjectFactory();
         RSCommonReply ws = f.createRSCommonReply();
 
@@ -126,7 +193,7 @@ public class JaxbPayloadFactory {
                 "Web service invocation was successful", message, entityId);
         ws.setReplyStatus(rst);
 
-        JaxbUtil util = new JaxbUtil("org.rmt2.jaxb");
+        JaxbUtil util = new JaxbUtil(MessagingConstants.JAXB_RMT2_PKG);
         String rawXml = util.marshalMessage(ws);
         return rawXml;
     }
