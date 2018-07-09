3f5440a0d71bab343a137cd5b0c129fdf31db450
DHISCO\roy.terrell <roy.terrell@dhisco.com>
2018-07-09 18:59:44 -0500

    IS-3:Added IP_INFO_GET transaction code and removed COUNTRY_REGION_GET.

--- src/main/java/org/rmt2/constants/ApiTransactionCodes.java
+++ src/main/java/org/rmt2/constants/ApiTransactionCodes.java
@@ -15,7 +15,7 @@ public class ApiTransactionCodes {
     public static final String ZIPCODE_GET = "GET_ZIPCODE";
     public static final String COUNTRY_GET = "GET_COUNTRY";
     public static final String REGION_GET = "GET_REGION";
-    public static final String COUNTRY_REGION_GET = "GET_COUNTRY_REGION";
+    public static final String IP_INFO_GET = "GET_IP_INFO";
 
     // MultiMedia
     public static final String MEDIA_GET_CONTENT = "GET_CONTENT_ATTACHMENT";
