a5938ccb77a82a5d09a562de3a98d1c3d105df90
appdev <royterrell2@gmail.com>
2020-01-20 18:54:56 -0600

    IS-28:  Added CLOSE_SALES_ORDER_WITH_PAYMENT transaction code.    Created test case that produces CLOSE_SALES_ORDER_WITH_PAYMENT request in XML.

---
+++ src/test/java/org/rmt2/messages/accounting/sales/SalesOrderCloseRequestBuilderTest.java
@@ -0,0 +1,96 @@
+package org.rmt2.messages.accounting.sales;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.rmt2.constants.ApiHeaderNames;
+import org.rmt2.constants.ApiTransactionCodes;
+import org.rmt2.constants.MessagingConstants;
+import org.rmt2.jaxb.AccountingTransactionRequest;
+import org.rmt2.jaxb.HeaderType;
+import org.rmt2.jaxb.ObjectFactory;
+import org.rmt2.jaxb.SalesOrderListType;
+import org.rmt2.jaxb.SalesOrderType;
+import org.rmt2.jaxb.TransactionDetailGroup;
+import org.rmt2.jaxb.XactLineitemType;
+import org.rmt2.jaxb.XactListType;
+import org.rmt2.jaxb.XactType;
+import org.rmt2.util.HeaderTypeBuilder;
+import org.rmt2.util.accounting.transaction.XactItemTypeBuilder;
+import org.rmt2.util.accounting.transaction.XactTypeBuilder;
+import org.rmt2.util.accounting.transaction.sales.SalesOrderTypeBuilder;
+
+import com.api.config.ConfigConstants;
+import com.api.config.SystemConfigurator;
+import com.api.util.RMT2Date;
+import com.api.xml.jaxb.JaxbUtil;
+
+public class SalesOrderCloseRequestBuilderTest {
+
+    private JaxbUtil jaxb;
+
+    @Before
+    public void setUp() throws Exception {
+        try {
+            jaxb = SystemConfigurator.getJaxb(ConfigConstants.JAXB_CONTEXNAME_DEFAULT);
+        } catch (Exception e) {
+            jaxb = new JaxbUtil(MessagingConstants.JAXB_RMT2_PKG);
+        }
+    }
+
+
+    @Test
+    public void testBuildRequest() {
+        ObjectFactory fact = new ObjectFactory();
+        AccountingTransactionRequest req = fact.createAccountingTransactionRequest();
+
+        HeaderType head = HeaderTypeBuilder.Builder.create().withApplication("accounting").withModule("transaction")
+                .withMessageMode(ApiHeaderNames.MESSAGE_MODE_REQUEST)
+                .withDeliveryDate(new Date())
+
+                // Set these header elements with dummy values in order to be
+                // properly assigned later.
+                .withTransaction(ApiTransactionCodes.ACCOUNTING_SALESORDER_CLOSE_WITH_PAYMENT)
+                .withRouting(ApiHeaderNames.DUMMY_HEADER_VALUE)
+                .withDeliveryMode(ApiHeaderNames.DUMMY_HEADER_VALUE).build();
+
+        SalesOrderType sot = SalesOrderTypeBuilder.Builder.create().withSalesOrderId(0).withCustomerId(3333)
+                .withCustomerName("ABC Company")
+                .withCustomerAcctNo("ACCT-NO-8888").withInvoiced(false).withOrderTotal(755.94)
+                .withEffectiveDate(RMT2Date.stringToDate("2020-01-01"))
+                .build();
+
+        TransactionDetailGroup data = fact.createTransactionDetailGroup();
+        SalesOrderListType silt = fact.createSalesOrderListType();
+        silt.getSalesOrder().add(sot);
+        data.setSalesOrders(silt);
+
+        List<XactLineitemType> items = new ArrayList<XactLineitemType>();
+        items.add(XactItemTypeBuilder.Builder.create().withXactTypeItemActvId(0).withAmount(755.94)
+                .withXactId(0).withItemId(601).withItemTypeId(7001)
+                .withDescription("item1").build());
+
+        XactType xt = XactTypeBuilder.Builder.create().withXactId(0).withXactTypeId(60).withXactAmount(755.94)
+                .withXactDate("2018-12-01").withReason("Test creating a transaction via message handler")
+                .withNegInstrNo("1111-1111-1111-1111").withTenderId(100).withXactItems(items).build();
+
+        XactListType xlt = fact.createXactListType();
+        xlt.getTransaction().add(xt);
+        data.setTransactions(xlt);
+
+        // Build transaction detail group
+        req.setProfile(data);
+
+        req.setHeader(head);
+
+        String xml = jaxb.marshalJsonMessage(req);
+        System.out.println(xml);
+        Assert.assertNotNull(xml);
+        Assert.assertTrue(xml.contains(ApiTransactionCodes.ACCOUNTING_SALESORDER_CLOSE_WITH_PAYMENT));
+    }
+
+}
