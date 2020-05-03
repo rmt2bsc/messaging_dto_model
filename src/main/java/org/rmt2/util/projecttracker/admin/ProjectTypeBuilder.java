8f281b235a68158b7327bf21bc1988c3a3c5d410
appdev <royterrell2@gmail.com>
2020-05-03 00:22:24 -0500

    IS-41:Created

---
+++ src/main/java/org/rmt2/util/projecttracker/admin/ProjectTypeBuilder.java
@@ -0,0 +1,217 @@
+package org.rmt2.util.projecttracker.admin;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.rmt2.jaxb.BusinessType;
+import org.rmt2.jaxb.ClientType;
+import org.rmt2.jaxb.CustomerType;
+import org.rmt2.jaxb.ObjectFactory;
+import org.rmt2.jaxb.ProjectType;
+import org.rmt2.jaxb.RecordTrackingType;
+import org.rmt2.util.accounting.subsidiary.CustomerTypeBuilder;
+import org.rmt2.util.addressbook.BusinessTypeBuilder;
+
+import com.api.util.RMT2Date;
+import com.api.util.assistants.Verifier;
+
+/**
+ * ProjectType Builder.
+ * 
+ * @author Roy Terrell
+ *
+ */
+public class ProjectTypeBuilder {
+
+    private ProjectType subject;
+    
+    /**
+     * Create a ProjectTypeBuilder
+     */
+    private ProjectTypeBuilder(Builder builder) {
+        ObjectFactory f = new ObjectFactory();
+        subject = f.createProjectType();
+
+        BusinessType bt = BusinessTypeBuilder.Builder.create()
+                .withBusinessId(builder.clientBusinessId)
+                .build();
+
+        CustomerType ct1 = CustomerTypeBuilder.Builder.create()
+                .withBusinessType(bt)
+                .build();
+
+        ClientType ct2 = ClientTypeBuilder.Builder.create()
+                .withClientId(builder.clientId)
+                .withClientName(builder.clientName)
+                .withCustomerData(ct1)
+                .withBillRate(builder.billRate)
+                .withOvertimeBillRate(builder.otBillRate)
+                .build();
+        subject.setClient(ct2);
+
+        subject.setProjectId(builder.projectId);
+        subject.setDescription(builder.projectName);
+        subject.setEffectiveDate(builder.effectiveDate);
+        subject.setTracking(builder.tracking);
+    }
+
+    /**
+     * Builder for {@link ProjectTypeBuilder}
+     * 
+     * @author Roy Terrell
+     *
+     */
+    public static final class Builder {
+        private int clientId;
+        private int clientBusinessId;
+        private String clientName;
+        private BigInteger projectId;
+        private String projectName;
+        private XMLGregorianCalendar effectiveDate;
+        private double billRate;
+        private double otBillRate;
+        private RecordTrackingType tracking;
+
+        private Builder() {
+            this.tracking = null;
+        }
+
+        /**
+         * Creates a new Builder.
+         * <p>
+         * Declared private to prevent direct instantiation by external
+         * consumers.
+         */
+        public static Builder create() {
+            return new Builder();
+        }
+
+        /**
+         * Set up client id.
+         * 
+         * @param clientId
+         *            an int value that must be greater than or equal to zero
+         * @return Non-null Builder used to continue building the object
+         * @throws VerifyException
+         *             if the parameter conditions are not met.
+         */
+        public Builder withClientId(int clientId) {
+            this.clientId = clientId;
+            return this;
+        }
+
+        /**
+         * Set up client business id.
+         * 
+         * @param businessId
+         *            an int value that must be greater than or equal to zero
+         * @return Non-null Builder used to continue building the object
+         * @throws VerifyException
+         *             if the parameter conditions are not met.
+         */
+        public Builder withClientBusinessId(int businessId) {
+            this.clientBusinessId = businessId;
+            return this;
+        }
+
+        /**
+         * Set up client name
+         * 
+         * @param name
+         *            an String value that must not be blank
+         * @return Non-null Builder used to continue building the object
+         * @throws VerifyException
+         *             if the parameter conditions are not met.
+         */
+        public Builder withClientName(String name) {
+            this.clientName = name;
+            return this;
+        }
+
+        /**
+         * Set up project id.
+         * 
+         * @param projectId
+         *            an int value
+         * @return Non-null Builder used to continue building the object
+         */
+        public Builder withProjectId(int projectId) {
+            this.projectId = BigInteger.valueOf(projectId);
+            return this;
+        }
+
+        /**
+         * Set up project name
+         * 
+         * @param name
+         *            an String value that must not be blank
+         * @return Non-null Builder used to continue building the object
+         */
+        public Builder withProjectName(String name) {
+            this.projectName = name;
+            return this;
+        }
+
+        /**
+         * Set up effective date
+         * 
+         * @param effectiveDate
+         * @return
+         */
+        public Builder withEffectiveDate(Date effectiveDate) {
+            this.effectiveDate = RMT2Date.toXmlDate(effectiveDate);
+            return this;
+        }
+        /**
+         * Set up bill rate.
+         * 
+         * @param billRate
+         *            an double value that must not be blank
+         * @return Non-null Builder used to continue building the object
+         */
+        public Builder withBillRate(double billRate) {
+            this.billRate = billRate;
+            return this;
+        }
+
+        /**
+         * Set up overtime bill rate.
+         * 
+         * @param otBillRate
+         *            an double value that must not be blank
+         * @return Non-null Builder used to continue building the object
+         */
+        public Builder withOvertimeBillRate(double otBillRate) {
+            this.otBillRate = otBillRate;
+            return this;
+        }
+
+        /**
+         * Set up Record Tracking Info.
+         * 
+         * @param tracking
+         *            an instance of {@link RecordTrackingType} value that must
+         *            not be blank
+         * @return Non-null Builder used to continue building the object
+         * @throws VerifyException
+         *             if the parameter conditions are not met.
+         */
+        public Builder withRecordTracking(RecordTrackingType tracking) {
+            Verifier.verifyNotNull(tracking, "Tracking info cannot be null");
+            this.tracking = tracking;
+            return this;
+        }
+
+        /**
+         * Completes the building of the ProjectType
+         * 
+         * @return an instance of {@link ProjectType}
+         */
+        public ProjectType build() {
+            ProjectTypeBuilder obj = new ProjectTypeBuilder(this);
+            return obj.subject;
+        }
+    }
+}
---
+++ src/test/java/org/rmt2/messages/projecttracker/admin/ProjectQueryRequestBuilderTest.java
@@ -0,0 +1,133 @@
+package org.rmt2.messages.projecttracker.admin;
+
+import java.math.BigInteger;
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.rmt2.constants.ApiHeaderNames;
+import org.rmt2.constants.ApiTransactionCodes;
+import org.rmt2.constants.MessagingConstants;
+import org.rmt2.jaxb.HeaderType;
+import org.rmt2.jaxb.ObjectFactory;
+import org.rmt2.jaxb.ProjectCriteriaGroup;
+import org.rmt2.jaxb.ProjectCriteriaType;
+import org.rmt2.jaxb.ProjectDetailGroup;
+import org.rmt2.jaxb.ProjectProfileRequest;
+import org.rmt2.jaxb.ProjectProfileResponse;
+import org.rmt2.jaxb.ProjectType;
+import org.rmt2.jaxb.RecordTrackingType;
+import org.rmt2.jaxb.ReplyStatusType;
+import org.rmt2.util.HeaderTypeBuilder;
+import org.rmt2.util.RecordTrackingTypeBuilder;
+import org.rmt2.util.ReplyStatusTypeBuilder;
+import org.rmt2.util.projecttracker.admin.ProjectTypeBuilder;
+
+import com.api.config.ConfigConstants;
+import com.api.config.SystemConfigurator;
+import com.api.util.RMT2Date;
+import com.api.xml.jaxb.JaxbUtil;
+
+public class ProjectQueryRequestBuilderTest {
+
+    private JaxbUtil jaxb;
+    
+    @Before
+    public void setUp() throws Exception {
+        try {
+            jaxb = SystemConfigurator.getJaxb(ConfigConstants.JAXB_CONTEXNAME_DEFAULT);
+        }
+        catch (Exception e) {
+            jaxb = new JaxbUtil(MessagingConstants.JAXB_RMT2_PKG);
+        }
+    }
+    
+    @Test
+    public void testBuildRequest() {
+        ObjectFactory fact = new ObjectFactory();
+        ProjectProfileRequest req = fact.createProjectProfileRequest();
+        
+        HeaderType head =  HeaderTypeBuilder.Builder.create()
+                .withApplication("projecttracker")
+                .withModule("admin")
+                .withMessageMode(ApiHeaderNames.MESSAGE_MODE_REQUEST)
+                .withDeliveryDate(new Date())
+                
+                // Set these header elements with dummy values in order to be properly assigned later.
+                .withTransaction(ApiTransactionCodes.PROJTRACK_PROJECT_GET)
+                .withRouting(ApiHeaderNames.DUMMY_HEADER_VALUE)
+                .withDeliveryMode(ApiHeaderNames.DUMMY_HEADER_VALUE).build();
+        
+        ProjectCriteriaType criteria = fact.createProjectCriteriaType();
+        criteria.setClientId(BigInteger.valueOf(200));
+        criteria.setClientName("ProjectTracker Client Name");
+        criteria.setClientBusinessId(BigInteger.valueOf(333));
+        criteria.setProjectId(BigInteger.valueOf(200));
+        criteria.setProjectName("Project Name");
+
+        ProjectCriteriaGroup pcg = fact.createProjectCriteriaGroup();
+        pcg.setProjectCriteria(criteria);
+        req.setCriteria(pcg);
+        req.setHeader(head);
+        
+        String xml = jaxb.marshalJsonMessage(req);
+        System.out.println(xml);
+        Assert.assertNotNull(xml);
+        Assert.assertTrue(xml.contains(ApiTransactionCodes.PROJTRACK_PROJECT_GET));
+    }
+
+    @Test
+    public void testBuildResponse() {
+        ObjectFactory fact = new ObjectFactory();
+        ProjectProfileResponse resp = fact.createProjectProfileResponse();
+
+        HeaderType head = HeaderTypeBuilder.Builder.create()
+                .withApplication("projecttracker")
+                .withModule("admin")
+                .withMessageMode(ApiHeaderNames.MESSAGE_MODE_REQUEST)
+                .withDeliveryDate(new Date())
+
+                // Set these header elements with dummy values in order to be
+                // properly assigned later.
+                .withTransaction(ApiTransactionCodes.PROJTRACK_PROJECT_GET)
+                .withRouting(ApiHeaderNames.DUMMY_HEADER_VALUE)
+                .withDeliveryMode(ApiHeaderNames.DUMMY_HEADER_VALUE).build();
+
+        ReplyStatusType rst = ReplyStatusTypeBuilder.Builder.create()
+                .withStatus(MessagingConstants.RETURN_STATUS_SUCCESS)
+                .withReturnCode(MessagingConstants.RETURN_CODE_SUCCESS)
+                .withMessage("project data found")
+                .withRecordCount(1)
+                .build();
+
+        RecordTrackingType tracking = RecordTrackingTypeBuilder.Builder.create()
+                .withDateCreated(RMT2Date.stringToDate("2020-01-01"))
+                .withDateUpdate(RMT2Date.stringToDate("2020-01-01"))
+                .withUserId("testuser")
+                .build();
+
+        ProjectType client = ProjectTypeBuilder.Builder.create()
+                .withClientId(200)
+                .withClientName("ProjectTracker Client Name")
+                .withClientBusinessId(7777)
+                .withBillRate(120.00)
+                .withOvertimeBillRate(130.00)
+                .withProjectId(10000)
+                .withProjectName("Project Name")
+                .withEffectiveDate(RMT2Date.stringToDate("2020/5/01"))
+                .withRecordTracking(tracking)
+                .build();
+
+        ProjectDetailGroup pdg = fact.createProjectDetailGroup();
+        pdg.getProject().add(client);
+        resp.setProfile(pdg);
+        resp.setHeader(head);
+        resp.setReplyStatus(rst);
+
+        String xml = jaxb.marshalJsonMessage(resp);
+        System.out.println(xml);
+        Assert.assertNotNull(xml);
+        Assert.assertTrue(xml.contains(ApiTransactionCodes.PROJTRACK_PROJECT_GET));
+    }
+}
