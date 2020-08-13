2ac27a1565a791830a470cef30d64989b7119b8a
appdev <royterrell2@gmail.com>
2020-08-13 18:25:57 -0500

    IS-51:  Completed the remainder of the GET Request unit test case

--- src/test/java/org/rmt2/messages/media/GenreQueryRequestBuilderTest.java
+++ src/test/java/org/rmt2/messages/media/GenreQueryRequestBuilderTest.java
@@ -1,6 +1,5 @@
 package org.rmt2.messages.media;
 
-import java.math.BigInteger;
 import java.util.Date;
 
 import org.junit.Assert;
@@ -9,12 +8,13 @@ import org.junit.Test;
 import org.rmt2.constants.ApiHeaderNames;
 import org.rmt2.constants.ApiTransactionCodes;
 import org.rmt2.constants.MessagingConstants;
-import org.rmt2.jaxb.CodeGroupType;
+import org.rmt2.jaxb.AudioVisualCriteriaType;
 import org.rmt2.jaxb.GenreType;
 import org.rmt2.jaxb.GenresType;
 import org.rmt2.jaxb.HeaderType;
-import org.rmt2.jaxb.LookupCodesRequest;
+import org.rmt2.jaxb.MimeCriteriaGroup;
 import org.rmt2.jaxb.MimeDetailGroup;
+import org.rmt2.jaxb.MultimediaRequest;
 import org.rmt2.jaxb.MultimediaResponse;
 import org.rmt2.jaxb.ObjectFactory;
 import org.rmt2.util.HeaderTypeBuilder;
@@ -41,29 +41,32 @@ public class GenreQueryRequestBuilderTest {
     @Test
     public void testBuildRequest() {
         ObjectFactory fact = new ObjectFactory();
-        LookupCodesRequest req = fact.createLookupCodesRequest();
+        MultimediaRequest req = fact.createMultimediaRequest();
         
         HeaderType head =  HeaderTypeBuilder.Builder.create()
-                .withApplication("addressbook")
+                .withApplication("media")
                 .withModule(ConfigConstants.API_APP_MODULE_VALUE)
                 .withMessageMode(ApiHeaderNames.MESSAGE_MODE_REQUEST)
                 .withDeliveryDate(new Date())
                 
                 // Set these header elements with dummy values in order to be properly assigned later.
-                .withTransaction(ApiTransactionCodes.LOOKUP_GROUP_UPDATE)
+                .withTransaction(ApiTransactionCodes.MEDIA_GENRE_GET)
                 .withRouting(ApiHeaderNames.DUMMY_HEADER_VALUE)
                 .withDeliveryMode(ApiHeaderNames.DUMMY_HEADER_VALUE).build();
         
-        CodeGroupType cgt = fact.createCodeGroupType();
-        cgt.setGroupId(BigInteger.valueOf(200));
-        cgt.setGroupDesc("Group Description Test");
-        req.getGroupCodes().add(cgt);
+        AudioVisualCriteriaType cgt = fact.createAudioVisualCriteriaType();
+        cgt.setGenreId(120);
+        cgt.setGenreName("Jazz");
+
+        MimeCriteriaGroup mcg = fact.createMimeCriteriaGroup();
+        mcg.setAudioVisualCriteria(cgt);
+        req.setCriteria(mcg);
         req.setHeader(head);
         
         String xml = jaxb.marshalJsonMessage(req);
         System.out.println(xml);
         Assert.assertNotNull(xml);
-        Assert.assertTrue(xml.contains(ApiTransactionCodes.LOOKUP_GROUP_UPDATE));
+        Assert.assertTrue(xml.contains(ApiTransactionCodes.MEDIA_GENRE_GET));
     }
  
     @Test
@@ -72,14 +75,14 @@ public class GenreQueryRequestBuilderTest {
         MultimediaResponse req = fact.createMultimediaResponse();
 
         HeaderType head = HeaderTypeBuilder.Builder.create()
-                .withApplication("mime")
+                .withApplication("media")
                 .withModule(ConfigConstants.API_APP_MODULE_VALUE)
                 .withMessageMode(ApiHeaderNames.MESSAGE_MODE_REQUEST)
                 .withDeliveryDate(new Date())
 
                 // Set these header elements with dummy values in order to be
                 // properly assigned later.
-                .withTransaction(ApiTransactionCodes.LOOKUP_GROUP_UPDATE)
+                .withTransaction(ApiTransactionCodes.MEDIA_GENRE_GET)
                 .withRouting(ApiHeaderNames.DUMMY_HEADER_VALUE)
                 .withDeliveryMode(ApiHeaderNames.DUMMY_HEADER_VALUE).build();
 
@@ -106,6 +109,6 @@ public class GenreQueryRequestBuilderTest {
         String xml = jaxb.marshalJsonMessage(req);
         System.out.println(xml);
         Assert.assertNotNull(xml);
-        Assert.assertTrue(xml.contains(ApiTransactionCodes.LOOKUP_GROUP_UPDATE));
+        Assert.assertTrue(xml.contains(ApiTransactionCodes.MEDIA_GENRE_GET));
     }
 }
