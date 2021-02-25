0cf6a22a3d4637990055bd97f48ad4e01bede5ea
appdev <royterrell2@gmail.com>
2021-02-24 19:24:11 -0600

    IS-56:  Added transaction code, GET_RESOURCE_TYPE

--- src/main/java/org/rmt2/constants/ApiTransactionCodes.java
+++ src/main/java/org/rmt2/constants/ApiTransactionCodes.java
@@ -160,6 +160,7 @@ public class ApiTransactionCodes {
     public static final String AUTH_ROLE_GET = "GET_ROLE";
     public static final String AUTH_ROLE_DELETE = "DELETE_ROLE";
     public static final String AUTH_RESOURCE_TYPE_UPDATE = "UPDATE_RESOURCE_TYPE";
+    public static final String AUTH_RESOURCE_TYPE_GET = "GET_RESOURCE_TYPE";
 
     public ApiTransactionCodes() {
         // TODO Auto-generated constructor stub
---
+++ src/test/java/org/rmt2/messages/authentication/ResourceTypeQueryRequestBuilderTest.java
@@ -0,0 +1,127 @@
+package org.rmt2.messages.authentication;
+
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.rmt2.constants.ApiHeaderNames;
+import org.rmt2.constants.ApiTransactionCodes;
+import org.rmt2.constants.MessagingConstants;
+import org.rmt2.jaxb.AuthCriteriaGroupType;
+import org.rmt2.jaxb.AuthProfileGroupType;
+import org.rmt2.jaxb.AuthenticationRequest;
+import org.rmt2.jaxb.AuthenticationResponse;
+import org.rmt2.jaxb.HeaderType;
+import org.rmt2.jaxb.ObjectFactory;
+import org.rmt2.jaxb.ResourceCriteriaType;
+import org.rmt2.jaxb.ResourcesInfoType;
+import org.rmt2.jaxb.ResourcetypeType;
+import org.rmt2.util.HeaderTypeBuilder;
+import org.rmt2.util.authentication.ResourcesInfoTypeBuilder;
+import org.rmt2.util.authentication.ResourcetypeTypeBuilder;
+
+import com.api.config.ConfigConstants;
+import com.api.config.SystemConfigurator;
+import com.api.xml.jaxb.JaxbUtil;
+
+public class ResourceTypeQueryRequestBuilderTest {
+
+    private JaxbUtil jaxb;
+    
+    @Before
+    public void setUp() throws Exception {
+        try {
+            jaxb = SystemConfigurator.getJaxb(ConfigConstants.JAXB_CONTEXNAME_DEFAULT);
+        }
+        catch (Exception e) {
+            jaxb = new JaxbUtil(MessagingConstants.JAXB_RMT2_PKG);
+        }
+    }
+    
+    @Test
+    public void testBuildRequest_New() {
+        ObjectFactory fact = new ObjectFactory();
+        AuthenticationRequest req = fact.createAuthenticationRequest();
+        
+        HeaderType head =  HeaderTypeBuilder.Builder.create()
+                .withApplication(ApiHeaderNames.APP_NAME_AUTHENTICATION)
+                .withModule("admin")
+                .withMessageMode(ApiHeaderNames.MESSAGE_MODE_REQUEST)
+                .withDeliveryDate(new Date())
+                
+                // Set these header elements with dummy values in order to be properly assigned later.
+                .withTransaction(ApiTransactionCodes.AUTH_RESOURCE_TYPE_GET)
+                .withRouting(ApiTransactionCodes.ROUTE_AUTHENTICATION)
+                .withDeliveryMode(ApiHeaderNames.DUMMY_HEADER_VALUE)
+                .withSessionId(ConfigConstants.API_DUMMY_SESSION_ID)
+                .build();
+
+        AuthCriteriaGroupType apgt = fact.createAuthCriteriaGroupType();
+        ResourceCriteriaType criteria = fact.createResourceCriteriaType();
+        criteria.setRsrcTypeId(1000);
+        criteria.setRsrcTypeName("Resource Type Name");
+
+        apgt.setResourceCriteria(criteria);
+
+        req.setCriteria(apgt);
+        req.setHeader(head);
+
+        String xml = jaxb.marshalJsonMessage(req);
+        System.out.println(xml);
+        Assert.assertNotNull(xml);
+        Assert.assertTrue(xml.contains(ApiTransactionCodes.AUTH_RESOURCE_TYPE_GET));
+    }
+
+    @Test
+    public void testBuildRespones() {
+        ObjectFactory fact = new ObjectFactory();
+        AuthenticationResponse resp = fact.createAuthenticationResponse();
+
+        HeaderType head = HeaderTypeBuilder.Builder.create()
+                .withApplication(ApiHeaderNames.APP_NAME_AUTHENTICATION)
+                .withModule("admin")
+                .withMessageMode(ApiHeaderNames.MESSAGE_MODE_REQUEST)
+                .withDeliveryDate(new Date())
+
+                // Set these header elements with dummy values in order to be
+                // properly assigned later.
+                .withTransaction(ApiTransactionCodes.AUTH_RESOURCE_TYPE_GET)
+                .withRouting(ApiTransactionCodes.ROUTE_AUTHENTICATION)
+                .withDeliveryMode(ApiHeaderNames.DUMMY_HEADER_VALUE)
+                .withSessionId(ConfigConstants.API_DUMMY_SESSION_ID)
+                .build();
+
+        AuthProfileGroupType apgt = fact.createAuthProfileGroupType();
+        ResourcetypeType rt1 = ResourcetypeTypeBuilder.Builder.create()
+                .withTypeId(777)
+                .withDescription("rsrouce type Name7")
+                .build();
+
+        ResourcetypeType rt2 = ResourcetypeTypeBuilder.Builder.create()
+                .withTypeId(778)
+                .withDescription("rsrouce type Name8")
+                .build();
+
+        ResourcetypeType rt3 = ResourcetypeTypeBuilder.Builder.create()
+                .withTypeId(779)
+                .withDescription("rsrouce type Name9")
+                .build();
+
+        ResourcesInfoType rit = ResourcesInfoTypeBuilder.Builder.create()
+                .addResourceType(rt1)
+                .addResourceType(rt2)
+                .addResourceType(rt3)
+                .build();
+
+        apgt.setResourcesInfo(rit);
+        resp.setProfile(apgt);
+        resp.setHeader(head);
+
+        String xml = jaxb.marshalJsonMessage(resp);
+        System.out.println(xml);
+        Assert.assertNotNull(xml);
+        Assert.assertTrue(xml.contains(ApiTransactionCodes.AUTH_RESOURCE_TYPE_GET));
+    }
+
+ }
