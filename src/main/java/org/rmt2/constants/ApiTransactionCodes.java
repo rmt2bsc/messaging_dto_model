4ab5903acce15a87966de140f0e3eeddb84a3219
appdev <royterrell2@gmail.com>
2021-03-02 13:13:49 -0600

    IS-57:  Added transaction code, DELETE_RESOURCE_SUB_TYPE

--- src/main/java/org/rmt2/constants/ApiTransactionCodes.java
+++ src/main/java/org/rmt2/constants/ApiTransactionCodes.java
@@ -164,6 +164,7 @@ public class ApiTransactionCodes {
     public static final String AUTH_RESOURCE_TYPE_DELETE = "DELETE_RESOURCE_TYPE";
     public static final String AUTH_RESOURCE_SUB_TYPE_UPDATE = "UPDATE_RESOURCE_SUB_TYPE";
     public static final String AUTH_RESOURCE_SUB_TYPE_GET = "GET_RESOURCE_SUB_TYPE";
+    public static final String AUTH_RESOURCE_SUB_TYPE_DELETE = "DELETE_RESOURCE_SUB_TYPE";
 
     public ApiTransactionCodes() {
         // TODO Auto-generated constructor stub
