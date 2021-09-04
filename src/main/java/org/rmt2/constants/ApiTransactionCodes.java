ce2de92ca939c29c783de289466fe118f3019199
appdev <royterrell2@gmail.com>
2021-09-04 13:54:55 -0500

    IS-63:  Added transaction code,  SSO_LOGIN_USER

--- src/main/java/org/rmt2/constants/ApiTransactionCodes.java
+++ src/main/java/org/rmt2/constants/ApiTransactionCodes.java
@@ -180,6 +180,7 @@ public class ApiTransactionCodes {
     public static final String AUTH_USER_APPROLE_MAINT = "MAINTAIN_USER_APPROLE";
     public static final String AUTH_USER_PERMISSIONS_GET = "GET_USER_PERMISSIONS";
     public static final String AUTH_USER_LOGIN = "LOGIN_USER";
+    public static final String AUTH_SSO_USER_LOGIN = "SSO_LOGIN_USER";
     public static final String AUTH_USER_LOGOUT = "LOGOUT_USER";
     public static final String AUTH_CHANGE_PASSWORD = "CHANGE_PASSWORD";
 
---
+++ src/test/java/org/rmt2/messages/authentication/SsoUserLoginRequestBuilderTest.java
@@ -0,0 +1,113 @@
+package org.rmt2.messages.authentication;
+
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.rmt2.constants.ApiHeaderNames;
+import org.rmt2.constants.ApiTransactionCodes;
+import org.rmt2.constants.MessagingConstants;
+import org.rmt2.jaxb.ApplicationAccessType;
+import org.rmt2.jaxb.AuthProfileGroupType;
+import org.rmt2.jaxb.AuthenticationRequest;
+import org.rmt2.jaxb.AuthenticationResponse;
+import org.rmt2.jaxb.HeaderType;
+import org.rmt2.jaxb.ObjectFactory;
+import org.rmt2.util.HeaderTypeBuilder;
+import org.rmt2.util.authentication.ApplicationAccessTypeBuilder;
+
+import com.api.config.ConfigConstants;
+import com.api.config.SystemConfigurator;
+import com.api.util.RMT2Date;
+import com.api.xml.jaxb.JaxbUtil;
+
+public class SsoUserLoginRequestBuilderTest {
+
+    private JaxbUtil jaxb;
+    
+    @Before
+    public void setUp() throws Exception {
+        try {
+            jaxb = SystemConfigurator.getJaxb(ConfigConstants.JAXB_CONTEXNAME_DEFAULT);
+        }
+        catch (Exception e) {
+            jaxb = new JaxbUtil(MessagingConstants.JAXB_RMT2_PKG);
+        }
+    }
+    
+    @Test
+    public void testBuildRequest() {
+        ObjectFactory fact = new ObjectFactory();
+        AuthenticationRequest req = fact.createAuthenticationRequest();
+        
+        HeaderType head =  HeaderTypeBuilder.Builder.create()
+                .withApplication(ApiHeaderNames.APP_NAME_AUTHENTICATION)
+                .withModule("auth")
+                .withMessageMode(ApiHeaderNames.MESSAGE_MODE_REQUEST)
+                .withDeliveryDate(new Date())
+                
+                // Set these header elements with dummy values in order to be properly assigned later.
+                .withTransaction(ApiTransactionCodes.AUTH_SSO_USER_LOGIN)
+                .withRouting(ApiTransactionCodes.ROUTE_AUTHENTICATION)
+                .withDeliveryMode(ApiHeaderNames.DUMMY_HEADER_VALUE)
+                .withSessionId(ConfigConstants.API_DUMMY_SESSION_ID)
+                .build();
+
+        AuthProfileGroupType apgt = fact.createAuthProfileGroupType();
+        ApplicationAccessType ul = ApplicationAccessTypeBuilder.Builder.create()
+                .withUserName("test_username")
+                .build();
+
+        apgt.getApplicationAccessInfo().add(ul);
+        req.setProfile(apgt);
+        req.setHeader(head);
+
+        String xml = jaxb.marshalJsonMessage(req);
+        System.out.println(xml);
+        Assert.assertNotNull(xml);
+        Assert.assertTrue(xml.contains(ApiTransactionCodes.AUTH_SSO_USER_LOGIN));
+    }
+
+    @Test
+    public void testBuildResponse() {
+        ObjectFactory fact = new ObjectFactory();
+        AuthenticationResponse resp = fact.createAuthenticationResponse();
+
+        HeaderType head = HeaderTypeBuilder.Builder.create()
+                .withApplication(ApiHeaderNames.APP_NAME_AUTHENTICATION)
+                .withModule("auth")
+                .withMessageMode(ApiHeaderNames.MESSAGE_MODE_RESPONSE)
+                .withDeliveryDate(new Date())
+
+                // Set these header elements with dummy values in order to be
+                // properly assigned later.
+                .withTransaction(ApiTransactionCodes.AUTH_SSO_USER_LOGIN)
+                .withRouting(ApiTransactionCodes.ROUTE_AUTHENTICATION)
+                .withDeliveryMode(ApiHeaderNames.DUMMY_HEADER_VALUE)
+                .withSessionId(ConfigConstants.API_DUMMY_SESSION_ID)
+                .build();
+
+        AuthProfileGroupType apgt = fact.createAuthProfileGroupType();
+        ApplicationAccessType ul = ApplicationAccessTypeBuilder.Builder.create()
+                .withUserName("test_username")
+                .withAppAccessId(3333)
+                .withLastLoginDate(RMT2Date.stringToDate("2021-01-01 12:00:00"))
+                .withSessionId("jkfdajskfda9e9w0fkdow9032u9fd0sifds")
+                .withLoginDate(new Date())
+                .withFirstName("Roy")
+                .withLastName("Terrell")
+                .withLoginInFlag(true)
+                .build();
+
+        apgt.getApplicationAccessInfo().add(ul);
+        resp.setProfile(apgt);
+        resp.setHeader(head);
+
+        String xml = jaxb.marshalJsonMessage(resp);
+        System.out.println(xml);
+        Assert.assertNotNull(xml);
+        Assert.assertTrue(xml.contains(ApiTransactionCodes.AUTH_SSO_USER_LOGIN));
+    }
+
+ }
